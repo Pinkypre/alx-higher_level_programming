7fd52bc3da6c2ad172d2c52fd1912529511cd77b
Pinkypre <preciousPncube@gmail.com>
2023-06-13 11:58:01 -0700

    updated

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,10 +2,4 @@
 
 
 def class_to_json(obj):
-    """ function that returns the dictionary
-    description
-    """
-    dic = {}
-    if hasattr(obj, "__dict__"):
-        dic = obj.__dict__.copy()
-    return dic
+    return vars(obj)
